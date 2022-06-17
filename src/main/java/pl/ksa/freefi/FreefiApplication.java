6c2b6a5b39417a3b928e4fad9e919f04c22b56e1
Maciek Saata <14015403+plblueraven@users.noreply.github.com>
2022-06-17 11:04:17 +0200

    Init repo

---
+++ src/main/java/pl/ksa/freefi/FreefiApplication.java
@@ -0,0 +1,13 @@
+package pl.ksa.freefi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreefiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreefiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ksa/freefi/FreefiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ksa.freefi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreefiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
